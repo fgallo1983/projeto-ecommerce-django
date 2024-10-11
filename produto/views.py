1aaad4e04b3300c57994800df118a26e80f12b82
Felipe Gallo <felipe_gallo@yahoo.com>
2024-10-11 16:13:41 -0300

    Montando o index

--- produto/views.py
+++ produto/views.py
@@ -5,10 +5,10 @@ from . import models
 from django.views.generic.detail import DetailView
 from django.http import HttpResponse
 
-class ListaProdutos(View):
-    def get(self, *args, **kwargs):
-        return HttpResponse('listar')
-
+class ListaProdutos(ListView):
+    model = models.Produto
+    template_name = 'produto/lista.html' 
+    
 class DetalheProduto(View):
     def get(self, *args, **kwargs):
         return HttpResponse('detalhes prouto')
