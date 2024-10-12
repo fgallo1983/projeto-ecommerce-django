5442dbe3370018b7d37fc49b464c20585e5c5e56
Felipe Gallo <felipe_gallo@yahoo.com>
2024-10-12 16:13:37 -0300

    Criando o card do produto

--- produto/models.py
+++ produto/models.py
@@ -1,8 +1,10 @@
-from django.db import models
-from PIL import Image
-import os
 from django.conf import settings
+import os
+from PIL import Image
+from django.db import models
 from django.utils.text import slugify
+from utils import utils
+
 
 class Produto(models.Model):
     nome = models.CharField(max_length=255)
@@ -22,7 +24,15 @@ class Produto(models.Model):
             ('S', 'Simples'),
         )
     )
-    
+
+    def get_preco_formatado(self):
+        return utils.formata_preco(self.preco_marketing)
+    get_preco_formatado.short_description = 'Preço'
+
+    def get_preco_promocional_formatado(self):
+        return utils.formata_preco(self.preco_marketing_promocional)
+    get_preco_promocional_formatado.short_description = 'Preço Promo.'
+
     @staticmethod
     def resize_image(img, new_width=800):
         img_full_path = os.path.join(settings.MEDIA_ROOT, img.name)
@@ -41,23 +51,23 @@ class Produto(models.Model):
             optimize=True,
             quality=50
         )
-        
+
     def save(self, *args, **kwargs):
-        
         if not self.slug:
             slug = f'{slugify(self.nome)}'
             self.slug = slug
-        
+
         super().save(*args, **kwargs)
-        
+
         max_image_size = 800
-        
+
         if self.imagem:
             self.resize_image(self.imagem, max_image_size)
-    
+
     def __str__(self):
         return self.nome
-    
+
+
 class Variacao(models.Model):
     produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
     nome = models.CharField(max_length=50, blank=True, null=True)
--- produto/views.py
+++ produto/views.py
@@ -8,6 +8,9 @@ from django.http import HttpResponse
 class ListaProdutos(ListView):
     model = models.Produto
     template_name = 'produto/lista.html' 
+    context_object_name = 'produtos'
+    paginate_by = 10
+    ordering = ['-id']
     
 class DetalheProduto(View):
     def get(self, *args, **kwargs):
