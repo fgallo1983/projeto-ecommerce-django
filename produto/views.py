5e911c8863497e15e038a0dea1cb15a65c1dfdfe
Felipe Gallo <felipe_gallo@yahoo.com>
2024-10-14 16:11:43 -0300

    Adicionar ao carrinho

--- produto/views.py
+++ produto/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse
 from django.views.generic.list import ListView
 from django.views import View
 from . import models
 from django.views.generic.detail import DetailView
 from django.http import HttpResponse
+from django.contrib import messages
 
 class ListaProdutos(ListView):
     model = models.Produto
@@ -20,7 +22,30 @@ class DetalheProduto(DetailView):
 
 class AdicionarAoCarrinho(View):
     def get(self, *args, **kwargs):
-        return HttpResponse('adiiconar ao carrinho')
+        http_referer = self.request.META.get(
+            'HTTP_REFERER',
+            reverse('produto:lista')
+        )
+        variacao_id = self.request.GET.get('vid')
+
+        if not variacao_id:
+            messages.error(
+                self.request,
+                'Produto não existe'
+            )
+            return redirect(http_referer)
+        
+        variacao = get_object_or_404(models.Variacao, id=variacao_id)
+        
+        if not self.request.session.get('carrinho'):
+            self.request.session['carrinho'] = {}
+            self.request.session.save()
+            
+        carrinho = self.request.session['carrinho']
+
+        if variacao_id in carrinho:
+            quantidade_carrinho = carrinho[variacao_id]['quantidade']
+            quantidade_carrinho += 1
 
 class RemoverDoCarrinho(View):
     def get(self, *args, **kwargs):
