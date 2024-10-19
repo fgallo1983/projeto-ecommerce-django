02262a2f5229d029b058f84116d605e9c61642ac
Felipe Gallo <felipe_gallo@yahoo.com>
2024-10-19 16:56:11 -0300

    Atualizar dados de usuario

--- perfil/views.py
+++ perfil/views.py
@@ -1,15 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import ListView
 from django.views import View
 from django.http import HttpResponse
 from . import models
 from . import forms
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+import copy
 
 class BasePerfil(View):
     template_name = 'perfil/criar.html'
 
     def setup(self, *args, **kwargs):
         super().setup(*args, **kwargs)
+        
+        self.carrinho = copy.deepcopy(self.request.session.get('carrinho', {}))
         
         self.perfil = None
         
@@ -26,6 +31,7 @@ class BasePerfil(View):
                 ),
                 'perfilform': forms.PerfilForm(
                     data=self.request.POST or None,
+                    instance=self.perfil
                 )
             }
         else:
@@ -41,6 +47,9 @@ class BasePerfil(View):
         self.userform = self.contexto['userform']
         self.perfilform = self.contexto['perfilform']
         
+        if self.request.user.is_authenticated:
+            self.template_name = 'perfil/atualizar.html'
+        
         self.renderizar = render(
             self.request, self.template_name, self.contexto)
         
@@ -55,11 +64,33 @@ class Criar(BasePerfil):
         username = self.userform.cleaned_data.get('username')
         password = self.userform.cleaned_data.get('password')
         email = self.userform.cleaned_data.get('email')
-        # first_name = self.userform.cleaned_data.get('first_name')
-        # last_name = self.userform.cleaned_data.get('last_name')
+        first_name = self.userform.cleaned_data.get('first_name')
+        last_name = self.userform.cleaned_data.get('last_name')
         
         if self.request.user.is_authenticated:
-            pass
+            usuario = get_object_or_404(
+                User, username=self.request.user.username) #type:ignore
+
+            usuario.username = username
+
+            if password:
+                usuario.set_password(password)
+
+            usuario.email = email
+            usuario.first_name = first_name
+            usuario.last_name = last_name
+            usuario.save()
+
+            if not self.perfil:
+                self.perfilform.cleaned_data['usuario'] = usuario
+                print(self.perfilform.cleaned_data)
+                perfil = models.Perfil(**self.perfilform.cleaned_data)
+                perfil.save()
+            else:
+                perfil = self.perfilform.save(commit=False)
+                perfil.usuario = usuario
+                perfil.save()
+                
         else:
             usuario = self.userform.save(commit=False)
             usuario.set_password(password)
@@ -68,6 +99,19 @@ class Criar(BasePerfil):
             perfil = self.perfilform.save(commit=False)
             perfil.usuario = usuario
             perfil.save()
+            
+        if password:
+            autentica = authenticate(
+                self.request,
+                username=usuario,
+                password=password
+            )
+
+            if autentica:
+                login(self.request, user=usuario)
+            
+        self.request.session['carrinho'] = self.carrinho
+        self.request.session.save()
             
         return self.renderizar
 
